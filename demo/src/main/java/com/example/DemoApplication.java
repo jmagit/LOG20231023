8235d138f64c49164b7b4d0b5867ef648a44e544
Javier <jmartin@grupoloyal.com>
2023-10-23 13:55:23 +0200

    Proyecto

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.ioc.Ejemplos;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Autowired
+	Ejemplos ejemplos;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Aplicacion arrancada");
+		ejemplos.run();
+	}
+
+}
---
+++ demo/src/main/java/com/example/ioc/Ejemplos.java
@@ -0,0 +1,10 @@
+package com.example.ioc;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ejemplos {
+	public void run() {
+		System.out.println("Soy el ejemplo");
+	}
+}
