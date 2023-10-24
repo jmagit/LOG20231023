54ba83f462340acbf541fe6a570d06bd6ef2d4af
Javier <jmartin@grupoloyal.com>
2023-10-24 09:42:23 +0200

    @Profile

--- demo/src/main/java/com/example/ioc/Configuracion.java
+++ demo/src/main/java/com/example/ioc/Configuracion.java
@@ -2,6 +2,7 @@ package com.example.ioc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class Configuracion {
@@ -26,12 +27,25 @@ public class Configuracion {
 	}
 	
 	@Bean
+	@Profile("default")
 	Tonteria dameTonteria() {
 		return new Tonteria() {
 			
 			@Override
 			public void dime() {
 				System.out.println("Soy una tonteria");
+			}
+		};
+	}
+	
+	@Bean
+	@Profile("test")
+	Tonteria dameTonteriaGrande() {
+		return new Tonteria() {
+			
+			@Override
+			public void dime() {
+				System.out.println("Soy una tonteria grande");
 			}
 		};
 	}
