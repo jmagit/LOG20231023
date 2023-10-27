c8eff5836105475898c14c3fb4effbd81134a1e9
Javier <jmartin@grupoloyal.com>
2023-10-27 09:32:55 +0200

    oper

--- demo/src/main/java/com/example/domains/contracts/repositories/PeliculasRespository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/PeliculasRespository.java
@@ -1,9 +1,19 @@
 package com.example.domains.contracts.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.domains.entities.Film;
 
+@RepositoryRestResource(path="peliculas", itemResourceRel="pelicula", collectionResourceRel="peliculas")
 public interface PeliculasRespository extends JpaRepository<Film, Integer> {
-
+	@RestResource(path = "cortos")
+	List<Film> findByLengthLessThan(int max);
+	
+	@Override
+	@RestResource(exported = false)
+	void deleteById(Integer id);
 }
